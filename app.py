80a1a527717cd4d09f4a87b20b411d22e8030c44
wiTTyMindsTech <63321726+wittymindstech@users.noreply.github.com>
2020-10-07 12:27:25 +0530

    Update app.py

--- app.py
+++ app.py
@@ -7,9 +7,14 @@ app = flask.Flask(__name__)
 app.config["MONGO_URI"] = "mongodb://15.207.109.209:27017/demo"
 mongo = PyMongo(app)
 
+
 print(mongo)
 
-
+@app.route('/mongo', methods=['GET'])
+def get_all_docs():
+  doc = mongo.db.abcd.insert({'abcd':'abcd'})
+  return "Inserted"
+  
 
 @app.route("/user/<username>")
 def user_profile(username):
@@ -18,5 +23,5 @@ def user_profile(username):
     return user
  
 
-
-app.run()
+if __name__ == "__main__":
+    app.run(debug=True)
