3e368818711eb750d4f4aeb4fc875617fbc9cca0
wiTTyMindsTech <63321726+wittymindstech@users.noreply.github.com>
2020-10-07 13:31:53 +0530

    Update app.py

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ import flask
 app = flask.Flask(__name__)
 
 #app = flask.Flask(__name__)
-app.config["MONGO_URI"] = "mongodb://15.207.109.209:27017/demo"
+app.config["MONGO_URI"] = "mongodb://13.233.199.195:27017/demo"
 mongo = PyMongo(app)
 
 
@@ -12,7 +12,7 @@ print(mongo)
 
 @app.route('/mongo', methods=['GET'])
 def get_all_docs():
-  doc = mongo.db.abcd.insert({'abcd':'abcd'})
+  doc = mongo.db.flask.insert({'location':'apac-blr'})
   return "Inserted"
   
 
